c0f03262a71b0538b07cd867519b3f9f272177ce
Hussain Shafiq <hussaynchaudhry@gmail.com>
2026-02-25 02:25:30 +0000

    fix:db

--- foodordering/settings.py
+++ foodordering/settings.py
@@ -151,21 +151,12 @@ WSGI_APPLICATION = 'foodordering.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-        'OPTIONS': {
-            'sslmode': 'require',  
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 AUTH_USER_MODEL = 'products.CustomUser'
 
 # Static files
